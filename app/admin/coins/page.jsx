a1e96b8460b4e3e6869187469a03bbc796687f55
benfran21 <benfran21@hotmail.com>
2025-05-15 19:22:47 -0400

    Se creo pagina de monedas

---
+++ app/admin/coins/page.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import Titulos from "@/components/Titulos"
+
+export default function coins() {
+  return (
+    <>
+      <Titulos texto="Monedas" />
+      <div>Monedas</div>
+    </>
+  )
+}
