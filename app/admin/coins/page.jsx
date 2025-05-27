ee9617f3b22cb8d4e0ebac85332f2e5d5ac00404
benfran21 <benfran21@hotmail.com>
2025-05-26 20:21:38 -0400

    Se creo formato para mostrar datos

--- app/admin/coins/page.jsx
+++ app/admin/coins/page.jsx
@@ -1,11 +1,54 @@
-import Link from "next/link";
-import Titulos from "@/components/Titulos"
+'use client'
 
-export default function coins() {
+import { useEffect, useState } from 'react'
+import Titulos from '@/components/Titulos'
+
+export default function Coins() {
+  const [tasas, setTasas] = useState({ USD: '', EUR: '' })
+
+  useEffect(() => {
+    async function obtenerTasas() {
+      try {
+        const res = await fetch('/api/bcv')
+        const json = await res.json()
+        if (json.tasas) {
+          setTasas(json.tasas)
+        }
+      } catch (err) {
+        console.error('Error al obtener tasas desde la API:', err)
+      }
+    }
+
+    obtenerTasas()
+  }, [])
+
   return (
     <>
       <Titulos texto="Monedas" />
-      <div>Monedas</div>
+      <div className="gap-2 grid">
+        <div className="justify-center content-center flex gap-2">
+          <p className="bg-slate-300 text-3xl p-1 border border-solid border-spacing-2 border-gray-400 block">
+            <strong className="text-5xl">$</strong> BCV:
+          </p>
+          <input
+            className="text-3xl p-1 border border-solid border-gray-400 block"
+            type="text"
+            readOnly
+            value={tasas.USD}
+          />
+        </div>
+        <div className="justify-center content-center flex gap-2">
+          <p className="bg-slate-300 text-3xl p-1 border border-solid border-spacing-2 border-gray-400 block">
+            <strong className="text-5xl">€</strong> BCV:
+          </p>
+          <input
+            className="text-3xl p-1 border border-solid border-gray-400 block"
+            type="text"
+            readOnly
+            value={tasas.EUR}
+          />
+        </div>
+      </div>
     </>
   )
 }
