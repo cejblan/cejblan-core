8c372fef581a1cd825ee332f7ff10f14044fa63c
benfran21 <benfran21@hotmail.com>
2025-06-18 12:22:48 -0400

    Se corrigio error para evitar inconvenientes

--- app/test-error/page.jsx
+++ app/test-error/page.jsx
@@ -3,4 +3,7 @@
 export default function ThrowError() {
   console.log("Esta es una pagina para crear un error intencionado y mostrar la pagina de error 500");
   'throw new Error("Este es un error 500 intencionado.");'
+  return (
+    <h1>Esta es una pagina para crear un error intencionado y mostrar la pagina de error 500</h1>
+  )
 }
