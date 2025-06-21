0a602eeb5185983aaa73d57b79d4c38cc91a3da7
benfran21 <benfran21@hotmail.com>
2025-06-21 17:15:12 -0400

    Se corrigio error al acceder a los datos

--- app/components/PrecioProducto.jsx
+++ app/components/PrecioProducto.jsx
@@ -4,13 +4,15 @@ import { useConversion } from '@/app/admin/components/Conversion';
 export default function PrecioProducto({ precio, format }) {
   const { convertir, conversionActiva, tasa } = useConversion();
 
-  const precioBs = convertir(precio);
+  const precioConvertido = convertir(precio);
+  const precioBase = Number(precio);
+  const precioBs = Number(precioConvertido);
 
   return (
     <>{
       format === 1 ?
         <h4 className="text-3xl text-purple-700 font-bold my-2">
-          ${precio}
+          ${precioBase.toFixed(2)}
           {conversionActiva && tasa && (
             <>
               <p className="bg-slate-300 text-slate-600 text-lg px-1 rounded-xl m-auto w-fit">
@@ -26,7 +28,7 @@ export default function PrecioProducto({ precio, format }) {
         conversionActiva && tasa ? (
           precioBs.toFixed(2) + "bs"
         ) : (
-          "$" + precio.toFixed(2)
+          "$" + precioBase.toFixed(2)
         )
     }
     </>
