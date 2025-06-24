9ecb69caee2b202fd31c3de8a45b8a72d260915c
benfran21 <benfran21@hotmail.com>
2025-06-24 11:21:26 -0400

    Se removio codigo imnecesario

--- app/api/wishlist/route.js
+++ app/api/wishlist/route.js
@@ -1,41 +1,6 @@
 import { conexion } from "@/libs/mysql";
 import { NextResponse } from "next/server";
 
-export async function GET(req, res) {
-  const { searchParams } = new URL(req.url);
-  const customerEmail = searchParams.get("customerEmail");
-
-  if (!customerEmail) {
-    return NextResponse.json({ error: "No se proporcionó el email del cliente" }, { status: 400 });
-  }
-
-  try {
-    // Obtener los IDs de los productos en la wishlist del cliente
-    const [wishlistItems] = await conexion.query(
-      "SELECT id FROM wishlist WHERE customer = ?",
-      [customerEmail]
-    );
-
-    if (wishlistItems.length === 0) {
-      return NextResponse.json([], { status: 200 }); // Si la wishlist está vacía, retornamos un arreglo vacío
-    }
-
-    const productIds = wishlistItems.map(item => item.id); // Extraer los IDs
-    // Obtener todos los detalles de los productos según los IDs
-    const [products] = await conexion.query(
-      "SELECT * FROM products WHERE id IN (?)",
-      [productIds]
-    );
-
-    return NextResponse.json(products, {
-      status: 200,
-    });
-  } catch (error) {
-    console.error("Error al obtener productos:", error);
-    return NextResponse.json({ error: "Error en el servidor" }, { status: 500 });
-  }
-}
-
 export async function POST(request) {
   try {
     const data = await request.formData();
