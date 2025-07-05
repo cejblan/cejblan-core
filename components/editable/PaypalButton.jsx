54ab893ce5206c912734500c45235144e903cdf6
benfran21 <benfran21@hotmail.com>
2025-07-04 20:58:47 -0400

    Se redujeron dimensiones

--- components/editable/PaypalButton.jsx
+++ components/editable/PaypalButton.jsx
@@ -4,7 +4,7 @@ export default function PaypalButton() {
 
   return (
     // ===START_RETURN===
-      <div id="paypalButton" className="bg-white hover:bg-blue-500 drop-shadow-6xl lg:w-8 max-[420px]:w-6 lg:h-8 max-[420px]:h-6 rounded-full z-40 fixed bottom-10 right-1">
+      <div id="paypalButton" className="bg-white hover:bg-blue-500 drop-shadow-6xl w-6 h-6 rounded-full z-40 fixed bottom-8 right-1">
         <a href="https://www.paypal.com/ncp/payment/SBGP7VBEAHDVE" target="_blank">
           <RiPaypalFill className="text-blue-500 hover:text-white w-full h-full py-1 m-auto" />
         </a>
--- components/editable/WhatsappButton.jsx
+++ components/editable/WhatsappButton.jsx
@@ -5,7 +5,7 @@ export default function WhatsappButton() {
 
   return (
     // ===START_RETURN===
-      <div id="whatsappButton" className="bg-green-500 hover:bg-white drop-shadow-6xl lg:w-8 max-[420px]:w-6 lg:h-8 max-[420px]:h-6 rounded-full z-40 fixed bottom-1 right-1">
+      <div id="whatsappButton" className="bg-green-500 hover:bg-white drop-shadow-6xl w-6 h-6 rounded-full z-40 fixed bottom-1 right-1">
         <Link href="https://api.whatsapp.com/send?text=Hola,%20informacion%20por%20favor&phone=584142245444" target="_blank">
           <RiWhatsappLine className="text-white hover:text-green-500 w-full h-full py-1 m-auto" />
         </Link>
