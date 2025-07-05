e9f5dd5b98faae7aa46643d47ac2edf8052eb040
benfran21 <benfran21@hotmail.com>
2025-07-04 21:40:22 -0400

    Se agregaron comentarios delimitadores

--- components/editable/Fondo.jsx
+++ components/editable/Fondo.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Fondo({ children }) {
   return (
+    // ===START_RETURN===
     <section className="bg-[url('https://9mtfxauv5xssy4w3.public.blob.vercel-storage.com/fondo01.jpg')] bg-cover bg-center">
       {children}
     </section>
+    // ===END_RETURN===
   )
 }
