56fc31e696c8124170cd67db2d2c42dc16cc5554
benfran21 <benfran21@hotmail.com>
2025-07-11 00:44:58 -0400

    Se movio codigo desarrollador

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,6 +1,3 @@
-import TruncateCart from "./components/TruncateCart";
-import TruncateWishlist from "./components/TruncateWishlist";
-
 export default function AdminPage() {
   return (
     <>
@@ -8,15 +5,7 @@ export default function AdminPage() {
         <h1 className="max-[420px]:text-2xl text-5xl font-bold">¡Bienvenido a CejBlan!</h1>
         <p className="max-[420px]:text-xl text-2xl font-semibold">Versión: 1.0.0</p>
       </div>
-      <div className="mb-4 max-[420px]:text-xs">
-        <div className="bg-white text-red p-2 rounded-xl shadow-6xl">
-          <h1 className="bg-blue-500 text-white text-lg font-bold py-1 max-[420px]:px-1 px-2 rounded-xl m-auto w-fit">Área de Mantenimiento</h1>
-          <div className="bg-white py-1 flex gap-1 justify-center items-center">
-            <TruncateCart />
-            <TruncateWishlist />
-          </div>
-        </div>
-      </div>
+      
     </>
   )
 }
