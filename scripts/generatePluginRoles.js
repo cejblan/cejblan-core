92f916f14156def758740bc011347cecc5ac040e
benfran21 <benfran21@hotmail.com>
2025-07-22 17:07:45 -0400

    Se corrige por error en vercel

--- scripts/generatePluginRoles.js
+++ scripts/generatePluginRoles.js
@@ -1,5 +1,5 @@
-import fs from "fs";
-import path from "path";
+const fs = require("fs");
+const path = require("path");
 
 const pluginsDir = path.join(process.cwd(), "app", "admin", "plugins");
 const outputPath = path.join(process.cwd(), "middleware.plugins.json");
