48f3630eba6268d4e23658e09b7151ac4a4f2291
benfran21 <benfran21@hotmail.com>
2025-08-16 17:41:47 -0400

    Se realizaron cambios menores

--- app/admin/store/page.jsx
+++ app/admin/store/page.jsx
@@ -380,7 +380,7 @@ export default function PluginStore() {
         )}
       </div>
 
-      {/* MODAL DETALLE (cuando el cliente abre el detalle sigue viéndose igual) */}
+      {/* MODAL DETALLE */}
       {selected && (
         <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 p-4">
           <div className="bg-white rounded-2xl shadow-2xl w-full max-w-3xl relative max-h-[90vh] overflow-y-auto">
