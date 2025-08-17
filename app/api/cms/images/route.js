8ff9fb26bbb85b7a204967a29c6429977665db5b
benfran21 <benfran21@hotmail.com>
2025-08-17 11:26:40 -0400

    Se removio cloudinary

--- app/api/cms/images/route.js
+++ app/api/cms/images/route.js
@@ -1,33 +1,3 @@
-/*
-import { NextResponse } from "next/server";
-import cloudinary from "@/libs/cloudinary";
-import { processImage } from "@/libs/processImage";
-
-export async function POST(request) {
-  try {
-    const data = await request.formData();
-    const image = data.get("image");
-    if (!image) return NextResponse.json({ error: 'No se recibió imagen' }, { status: 400 });
-
-    const buffer = await processImage(image);
-    const res = await new Promise((resolve, reject) => {
-      cloudinary.uploader.upload_stream(
-        { resource_type: "image" },
-        (err, result) => {
-          if (err) return reject(err);
-          resolve(result);
-        }
-      ).end(buffer);
-    });
-
-    return NextResponse.json({ secure_url: res.secure_url });
-  } catch (err) {
-    console.error("Error al subir imagen:", err);
-    return NextResponse.json({ error: 'Error interno' }, { status: 500 });
-  }
-}
-*/
-
 import { list, put, del } from '@vercel/blob';
 import { NextResponse } from 'next/server';
 
