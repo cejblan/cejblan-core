36336a862abac4c116b9e07a6d71ee3beef6cb34
Francisco Gonzalez <84050491+cejblan@users.noreply.github.com>
2025-08-26 15:40:58 -0400

    (CMS) Delete page Prueba

--- components/pages/Prueba.jsx
+++
@@ -1,8 +0,0 @@
-export default function Prueba() {
-  return (
-    // ===START_RETURN===
-    
-    <div>Descripción de la prueba a ver si vercel despliega la pagina</div>
-    // ===END_RETURN===
-  )
-}
